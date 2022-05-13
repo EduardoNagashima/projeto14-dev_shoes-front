22fc49170e130b1a2fca23bb6ede053b38e5f2e0
Eduardo Nagashima <eduyudji@gmail.com>
2022-05-12 23:22:55 -0300

    footer quase pronto

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,41 +1,43 @@
 import { FooterDiv, FooterContainer, UserDiv } from "./style";
 import UserContext from "../Contexts/UserContext";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Footer() {
-
     const { token } = useContext(UserContext);
-
+    const navigate = useNavigate();
     const [usuario, setUsuario] = useState({
         nome: 'Fulano',
         foto: 'https://i1.sndcdn.com/avatars-000600452151-38sfei-t240x240.jpg'
     })
+
     let footer =
         <FooterContainer>
             <FooterDiv>
-                <ion-icon name="person-outline"></ion-icon>
-                <ion-icon name="heart-outline"></ion-icon>
+                <Link to={"/login"}><ion-icon name="person-outline"></ion-icon></Link>
+                <Link to={"/carrinho"}><ion-icon name="cart-outline"></ion-icon></Link>
             </FooterDiv>
         </FooterContainer>
 
-    useEffect(() => {
-        if (!token) {
-            footer =
-                <FooterContainer>
-                    <FooterDiv>
-                        <UserDiv>
-                            <img src={usuario.foto} alt="foto-de-usuario" />
-                            <p>{usuario.nome}</p>
-                        </UserDiv>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="log-out-outline"></ion-icon>
-                    </FooterDiv>
-                </FooterContainer>
-        }
-    })
+    function logout() {
+        localStorage.removeItem("token");
+        navigate("/");
+    }
 
-    return (
-        footer
-    );
+    if (token) {
+        footer =
+            <FooterContainer>
+                <FooterDiv>
+                    <UserDiv>
+                        <img src={usuario.foto} alt="foto-de-usuario" />
+                        <p>{usuario.nome}</p>
+                    </UserDiv>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="cart-outline"></ion-icon>
+                    <ion-icon onClick={logout} name="log-out-outline"></ion-icon>
+                </FooterDiv>
+            </FooterContainer>
+    }
+
+    return footer;
 }
